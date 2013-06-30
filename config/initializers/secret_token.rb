84eed34b1b58bea1429d5d06c85d8db1d25e5cef
musik <860577@gmail.com>
2013-06-30 21:18:26 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Douqi::Application.config.secret_token = 'ed64313ebea253bff94d4fae39a8ed564f3f3f882efc6d7825f4773971f60fddfad50f26864b6645404d6003d16cdb90ba9b234fe64f456d697afd16ba1f5607'
